bb5760b1d599d9b48c9b3c54a7dfaba771d0335e
Chris West (Faux) <git@goeswhere.com>
2018-10-08 17:18:11 +0100

    nih input solution

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,20 @@ extern crate serde_derive;
 extern crate failure;
 extern crate nix;
 extern crate regex;
-extern crate termion;
 extern crate toml;
 extern crate xcb;
 
 use std::env;
-use std::io;
-use std::io::Read;
-use std::thread;
+use std::sync::mpsc;
+use std::time::Duration;
 
 mod config;
+mod term;
 mod x;
 
 use failure::Error;
 use failure::ResultExt;
-use termion::raw::IntoRawMode;
+use nix::sys::termios;
 
 use config::Config;
 
@@ -42,8 +41,9 @@ fn main() -> Result<(), Error> {
 
     let mut conn = x::XServer::new()?;
 
-    let _stdout = io::stdout().into_raw_mode().unwrap();
-    let mut stdin = termion::async_stdin();
+    let _drop =
+        term::StdOutTermios::with_setup(|attr| attr.local_flags ^= termios::LocalFlags::ICANON)?;
+    let stdin = term::async_stdin();
 
     let mut last_procs = Vec::new();
 
@@ -57,46 +57,41 @@ fn main() -> Result<(), Error> {
 
         if procs != last_procs {
             println!();
-            println!("Waiting for: {}\r", compressed_list(&procs));
-            println!("Action? [d]elete, [t]erm, [k]ill, [q]uit)?\r");
+            println!("Waiting for: {}", compressed_list(&procs));
+            println!("Action? [d]elete, [t]erm, [k]ill, [q]uit)? ");
             last_procs = procs.clone();
         }
 
-        let mut buf = [0u8; 32];
-        let found = stdin.read(&mut buf)?;
-        let buf = &buf[..found];
-        for cmd in buf {
-            const CTRL_C: u8 = b'C' - 0x40;
-            const CTRL_D: u8 = b'D' - 0x40;
-            const CTRL_Z: u8 = b'Z' - 0x40;
-            match *cmd {
-                b'd' => {
-                    println!("Asking everyone to quit.\r");
-                    for proc in &procs {
-                        conn.delete_window(proc.window)?;
-                    }
+        match stdin.recv_timeout(Duration::from_millis(50)) {
+            Ok(b'd') => {
+                println!("Asking everyone to quit.");
+                for proc in &procs {
+                    conn.delete_window(proc.window)?;
                 }
-                b't' => {
-                    println!("Telling everyone to quit.\r");
-                    for proc in &procs {
-                        let _ = kill(proc.pid, Some(nix::sys::signal::SIGTERM))?;
-                    }
+            }
+            Ok(b't') => {
+                println!("Telling everyone to quit.");
+                for proc in &procs {
+                    let _ = kill(proc.pid, Some(nix::sys::signal::SIGTERM))?;
                 }
-                b'k' => {
-                    println!("Quitting everyone.\r");
-                    for proc in &procs {
-                        let _ = kill(proc.pid, Some(nix::sys::signal::SIGKILL))?;
-                    }
+            }
+            Ok(b'k') => {
+                println!("Quitting everyone.");
+                for proc in &procs {
+                    let _ = kill(proc.pid, Some(nix::sys::signal::SIGKILL))?;
                 }
-                b'q' | CTRL_C | CTRL_D | CTRL_Z => {
-                    println!("User asked, exiting\r");
-                    break 'app;
-                }
-                other => println!("unsupported command: {:?}\r", other as char),
+            }
+            Ok(b'q') => {
+                println!("User asked, exiting");
+                break 'app;
+            }
+            Ok(other) => println!("unsupported command: {:?}", other as char),
+            Err(mpsc::RecvTimeoutError::Timeout) => (),
+            Err(mpsc::RecvTimeoutError::Disconnected) => {
+                println!("End of commands, exiting");
+                break 'app;
             }
         }
-
-        sleep_ms(50);
     }
 
     Ok(())
@@ -109,7 +104,10 @@ fn find_procs(config: &Config, conn: &mut x::XServer) -> Result<Vec<Proc>, Error
         match gather_window_details(&config, conn, window_id) {
             Ok(Some(proc)) => procs.push(proc),
             Ok(None) => (),
-            Err(e) => eprintln!("couldn't get details (window vanished?): {:?} {:?}", window_id, e),
+            Err(e) => eprintln!(
+                "couldn't get details (window vanished?): {:?} {:?}",
+                window_id, e
+            ),
         }
         Ok(())
     })?;
@@ -146,7 +144,8 @@ fn gather_window_details(
     conn: &x::XServer,
     window: x::XWindow,
 ) -> Result<Option<Proc>, Error> {
-    let class = conn.read_class(window)
+    let class = conn
+        .read_class(window)
         .with_context(|_| format_err!("finding class of {:?}", window))?;
 
     for ignore in &config.ignore {
@@ -155,7 +154,8 @@ fn gather_window_details(
         }
     }
 
-    let pids = conn.pids(window)
+    let pids = conn
+        .pids(window)
         .with_context(|_| format_err!("finding pid of {:?} ({:?})", class, window))?;
 
     let pid = match pids.len() {
@@ -181,7 +181,8 @@ fn gather_window_details(
     Ok(Some(Proc {
         window,
         pid,
-        supported_protocols: conn.supported_protocols(window)
+        supported_protocols: conn
+            .supported_protocols(window)
             .with_context(|_| format_err!("finding protocols of {:?} (pid {})", class, pid))?,
         class,
     }))
@@ -198,10 +199,6 @@ fn kill(pid: u32, signal: Option<nix::sys::signal::Signal>) -> Result<bool, Erro
         Err(nix::Error::Sys(Errno::ESRCH)) => false,
         other => bail!("kill {} failed: {:?}", pid, other),
     })
-}
-
-fn sleep_ms(ms: u64) {
-    thread::sleep(::std::time::Duration::from_millis(ms))
 }
 
 #[cfg(test)]
---
+++ src/term.rs
@@ -0,0 +1,62 @@
+use std::io;
+use std::io::Read;
+use std::os::unix::io::AsRawFd;
+use std::os::unix::io::RawFd;
+use std::sync::mpsc;
+use std::thread;
+
+use failure::Error;
+use nix::sys::termios;
+
+pub struct StdOutTermios {
+    fd: RawFd,
+    original: termios::Termios,
+}
+
+impl StdOutTermios {
+    pub fn with_setup<F>(func: F) -> Result<StdOutTermios, Error>
+    where
+        F: FnOnce(&mut termios::Termios),
+    {
+        let out = io::stdout();
+        let fd = out.as_raw_fd();
+        let original = termios::tcgetattr(fd)?;
+        let mut updated = original.clone();
+        func(&mut updated);
+        termios::tcsetattr(fd, termios::SetArg::TCSANOW, &updated)?;
+        Ok(StdOutTermios { fd, original })
+    }
+}
+
+impl Drop for StdOutTermios {
+    fn drop(&mut self) {
+        let _ = termios::tcsetattr(self.fd, termios::SetArg::TCSANOW, &self.original);
+    }
+}
+
+pub fn async_stdin() -> mpsc::Receiver<u8> {
+    let (tx, rx) = mpsc::sync_channel(0);
+    thread::spawn(move || {
+        loop {
+            let mut buf = [0u8; 16];
+            let found = io::stdin().read(&mut buf).unwrap();
+            if 0 == found {
+                // implicit EOF
+                break;
+            }
+
+            let buf = &buf[..found];
+            for byte in buf {
+                if 4 == *byte {
+                    // We're assuming this is a ctrl+d, exit now
+                    // TODO: presumably this can occur in the middle of other streams
+                    // TODO: doesn't matter too much for the "ask for a single letter" case
+                    break;
+                }
+
+                tx.send(*byte).unwrap();
+            }
+        }
+    });
+    rx
+}
